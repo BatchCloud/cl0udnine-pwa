b2878fae9c68fc538c31653553b25d535f68ea13
Sebastian Steininger <batchcloud@web.de>
2023-02-04 13:18:57 +0100

    update

---
+++ public/dist/worker.js
@@ -0,0 +1,30 @@
+const VERSION = '1.0.0';
+
+self.addEventListener('install', self.skipWaiting);
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(caches.keys().then((keys) => {
+        return Promise.all(keys.map((key) => {
+            if(key === VERSION) return;
+            return caches.delete(key);
+        }));
+    }));
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith((async () => {
+        const file = await caches.match(event.request);
+        if(file) return file;
+        try {
+            const response = await fetch(event.request);
+            if(!event.request.url.includes('/api')) {
+                const cache = await caches.open(VERSION);
+                cache.put(event.request, response.clone());
+            }
+            return response;
+        }
+        catch(error) {
+            console.error(error);
+        }
+    })());
+});
