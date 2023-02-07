ddbd57acde317761e16936c2179154c536b6a222
Sebastian Steininger <batchcloud@web.de>
2023-02-07 19:59:17 +0100

    uzp

--- public/worker.js
+++ public/worker.js
@@ -29,7 +29,7 @@
 //     })());
 // });
 
-const version = '1.1.2';
+const version = '1.1.3';
 let staticName = `staticCache-${version}`;
 let dynamicName = `dynamicCache`;
 let imageName = `imageCache-${version}`;
